dce908a44323489e193ce7a9743ed78b9c6ef4ff
Caitlin Stein <caitlinstein@Caitlins-MacBook-Air.local>
2015-09-17 11:01:15 -0500

    updated .gitignore

---
+++ app/filters/searchAttributes.js
@@ -0,0 +1,24 @@
+app.filter('searchAttributes', function() {
+   
+  return function(piece, attributes) {
+    var filtered = [];
+    if (attributes === undefined) {
+      return piece;
+      } else {
+      angular.forEach(piece, function(title) {
+        angular.forEach(attributes, function(value, key, obj) {
+          if (title[key] === value && containsObject(title, filtered) === false) {
+            filtered.push(title);
+          } 
+        });
+      });
+    console.log("attributes", attributes);
+    return filtered;
+    }
+  }
+});
+
+function containsObject(obj, list) {
+ var res = _.find(list, function(val){ return _.isEqual(obj, val)});
+ return (_.isObject(res))? true:false;
+}
