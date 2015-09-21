cfb5c6dacdb010d4843924bfd6127f86262b2582
Caitlin Stein <caitlinstein@Caitlins-MacBook-Air.local>
2015-09-21 11:12:17 -0500

    detail route works

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -4,23 +4,13 @@ app.controller("DetailCtrl", ["$scope", "$routeParams", "$firebaseObject",
     $scope.pieceId = $routeParams.pieceId;
     $scope._ = _;
 
-    console.log("Did we even get here?");
-    var ref = new Firebase("https://band-library.firebaseio.com/");
-    $scope.piece = new $firebaseObject(ref.child('pieces/' + $scope.pieceId));
 
-
-    // $scope.selectedPiece = _.findKey($scope.pieces, {'$id': $scope.pieceID});
-
-    // $scope.selectedPiece = pieces.filter(function(piece) {
-    //   return piece.$id === parseInt($scope.songId);
-    // })[0];
-  
-  $scope.selectedPiece = $scope.pieces[$scope.pieceId];
+    var ref = new Firebase("https://band-library.firebaseio.com/pieces/" + $scope.pieceId );
+    $scope.selectedPiece = $firebaseObject(ref);
 
 
 
     console.log("$scope.pieceId", $scope.pieceId);
-    console.log("$scope.pieces", $scope.pieces);
     console.log("$scope.selectedPiece", $scope.selectedPiece);
 
       
