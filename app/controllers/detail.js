a8c2996935711571896d40a19ef36fbad64073f4
Caitlin Stein <caitlinstein@Caitlins-MacBook-Air.local>
2015-09-21 11:55:35 -0500

    edit page pushes changes to firebase

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,5 +1,5 @@
-app.controller("DetailCtrl", ["$scope", "$routeParams", "$firebaseObject",
-  function($scope, $routeParams, $firebaseObject) {
+app.controller("DetailCtrl", ["$scope", "$routeParams", "$firebaseObject", "$firebase",
+  function($scope, $routeParams, $firebaseObject, $firebase) {
     $scope.selectedPiece = {};
     $scope.pieceId = $routeParams.pieceId;
     $scope._ = _;
@@ -13,8 +13,25 @@ app.controller("DetailCtrl", ["$scope", "$routeParams", "$firebaseObject",
     console.log("$scope.pieceId", $scope.pieceId);
     console.log("$scope.selectedPiece", $scope.selectedPiece);
 
+     $scope.submit = function(obj) {
+       obj.$save().then(function(ref) {
+        console.log("Saved??");
+    //     ref.key() === obj.$id;
+    //     }
+    //     }
+       });
+     };
+
       
       
 
   }
 ]);
+
+// var obj = $firebaseObject(ref); (selected piece)
+// obj.foo = "bar";
+// obj.$save().then(function(ref) {
+//   ref.key() === obj.$id; // true
+// }, function(error) {
+//   console.log("Error:", error);
+// });
